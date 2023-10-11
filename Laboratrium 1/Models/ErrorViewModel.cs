9807394be13cb8a538364803ae7dccd6c551036f
kordian.klys <kordian.klys@SF-07>
2023-10-11 14:29:40 +0200

    Add project files.

---
+++ Laboratrium 1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Laboratrium_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
