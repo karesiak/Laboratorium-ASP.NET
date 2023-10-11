1330d05fe43140b58953491872ec0bc6f7d75c73
karesiak <kordek0945@gmail.com>
2023-10-11 15:38:23 +0200

    11.10 zadanie

---
+++ Laboratorium 2/Controllers/BirthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratorium_2.Controllers
+{
+    public class BirthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult DateofBirth()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Laboratorium 2/Models/Birth.cs
@@ -0,0 +1,13 @@
+﻿namespace Laboratorium_2.Models
+{
+    public class Birth
+    {
+
+        public string? UserName { get; set; }
+        public int?  Age{ get; set; }
+
+       
+        
+
+    }
+}
